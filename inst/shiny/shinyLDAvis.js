57e0021eff3f0b472a2037499e3c2cc027be17b1
tguo-1 <75446446+tguo-1@users.noreply.github.com>
2020-12-04 08:30:19 +0000

    Update shinyLDAvis.js

--- inst/shiny/shinyLDAvis.js
+++ inst/shiny/shinyLDAvis.js
@@ -7,7 +7,7 @@ ldavisBinding.find = function(scope) {
 ldavisBinding.renderValue = function(el, data) {
   // remove the old graph 
   // http://stackoverflow.com/questions/14422198/how-do-i-remove-all-children-elements-from-a-node-and-them-apply-them-again-with
-  var old_plot = d3.select(el).selectAll("*").remove();
+  var old_plot = d3v3.select(el).selectAll("*").remove();
   // add the new plot
   var json_file = "ldavisAssets/" + data.jsonFile;
   var to_select = "#" + el.id;
